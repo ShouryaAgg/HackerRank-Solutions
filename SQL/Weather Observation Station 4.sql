e4f1e66d37b3568c8adc18c97ce8f11f5d831268
Shourya Agarwal <shourya052003@gmail.com>
2025-11-24 05:28:09 +0530

    Added Solution of Weather Observation Station 4

--- SQL/Weather Observation Station 4.sql
+++ SQL/Weather Observation Station 4.sql
@@ -1,8 +1,3 @@
-SELECT * FROM city WHERE countrycode = "JPN";SELECT name FROM city WHERE countrycode = "JPN";SELECT * FROM city WHERE countrycode = "USA" and population > 100000;SELECT name FROM city WHERE countrycode = "USA" AND population > 120000;SELECT * FROM city WHERE countrycode = "JPN";SELECT name FROM city WHERE countrycode = "JPN";SELECT * FROM city WHERE countrycode = "USA" and population > 100000;SELECT name FROM city WHERE countrycode = "USA" AND population > 120000;SELECT * FROM city WHERE id = 1661;SELECT city,state FROM station;SELECT city 
-FROM station 
-WHERE id % 2 =0
-GROUP BY city;SELECT * FROM city WHERE id = 1661;
-SELECT city,state FROM station;SELECT * FROM city WHERE countrycode = "JPN";SELECT name FROM city WHERE countrycode = "JPN";SELECT * FROM city WHERE countrycode = "USA" and population > 100000;SELECT name FROM city WHERE countrycode = "USA" AND population > 120000;SELECT * FROM city WHERE countrycode = "JPN";SELECT name FROM city WHERE countrycode = "JPN";SELECT * FROM city WHERE countrycode = "USA" and population > 100000;SELECT name FROM city WHERE countrycode = "USA" AND population > 120000;SELECT * FROM city WHERE id = 1661;SELECT city,state FROM station;SELECT city 
-FROM station 
-WHERE id % 2 =0
-GROUP BY city;SELECT * FROM city WHERE id = 1661;SELECT city,state FROM station;
+SELECT 
+    count(city)- count(distinct city) 
+FROM station;
