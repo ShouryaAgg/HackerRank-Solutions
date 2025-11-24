f73c35c9cfa81e04307562d8ca83a7721b2a75c5
Shourya Agarwal <shourya052003@gmail.com>
2025-11-24 05:30:55 +0530

    Added Solution of Select All

--- SQL/Select All.sql
+++ SQL/Select All.sql
@@ -1,4 +1,1 @@
-SELECT * FROM city WHERE countrycode = "JPN";SELECT name FROM city WHERE countrycode = "JPN";SELECT * FROM city WHERE countrycode = "USA" and population > 100000;SELECT name FROM city WHERE countrycode = "USA" AND population > 120000;SELECT * FROM city WHERE id = 1661;SELECT city,state FROM station;SELECT city 
-FROM station 
-WHERE id % 2 =0
-GROUP BY city;
+SELECT * FROM city;
